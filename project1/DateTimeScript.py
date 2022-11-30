19ca1ca02c88225c74780810760bea94991fad3a
kanaverd <112012293+kanaverd@users.noreply.github.com>
2022-11-30 17:32:26 -0500

    Delete DateTimeScript.py

--- project1/DateTimeScript.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-a = datetime.datetime.now()
-
-print ("Today's date is: = %s/%s/%s" % (a.month, a.day, a.year))
-
-print ("The time is now: = %s:%s:%s" % (a.hour, a.minute, a.second))
